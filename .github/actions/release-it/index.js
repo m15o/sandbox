5df910db016fb8f404629d46a0d82c3450c95d5d
masanori_matsumoto <masanori_matsumoto@cybozu.co.jp>
2019-11-22 09:40:05 +0900

    log env variables

--- .github/actions/release-it/index.js
+++ .github/actions/release-it/index.js
@@ -1,6 +1,11 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const github_sha=process.env.GITHUB_SHA
+const github_ref=process.env.GITHUB_REF
+console.log(`inputs: GITHUB_SHA=${github_sha}`);
+console.log(`inputs: GITHUB_REF=${github_ref}`);
+
 try {
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
