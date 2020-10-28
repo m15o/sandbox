9692db951b77d9b2a8021cd7839c2294b0604bc7
masanori_matsumoto <masanori_matsumoto@cybozu.co.jp>
2020-10-28 16:19:42 +0900

    add xerrors

--- myproj/main.go
+++ myproj/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 
+	"golang.org/x/xerrors"
 	"sandbox/mylib"
 )
 
+func Hello() error {
+	return xerrors.New("hello")
+}
+
 func main() {
+	fmt.Printf("Hello: %v\n", Hello())
 	fmt.Printf("mylib.Hello: %v\n", mylib.Hello())
 }
